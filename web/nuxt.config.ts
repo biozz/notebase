1ba7f7cb19bcc8703ac34df948c303d628bf5481
Mikhail <meha6699@gmail.com>
2025-05-08 21:33:13 +0300

    add missing

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,21 +1,10 @@
 export default defineNuxtConfig({
   modules: ['@vueuse/nuxt', '@pinia/nuxt', '@nuxt/eslint', '@nuxt/ui'],
   ssr: false,
-
   imports: {
     autoImport: false,
   },
   devtools: { enabled: true },
-  app: {
-    head: {
-      link: [
-        {
-          rel: 'stylesheet',
-          href: 'https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css',
-        },
-      ],
-    },
-  },
   css: ['~/assets/css/main.css'],
   router: {
     options: {
@@ -27,6 +16,10 @@ export default defineNuxtConfig({
       apiBase: 'http://127.0.0.1:8090', // NUXT_PUBLIC_API_BASE=/ in production
     },
   },
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-11-01',
   nitro: {
     output: {
       // { dir: '.output', serverDir: '.output/server', publicDir: '.output/public' }
