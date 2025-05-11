54cb84004103581a32c5e5667d1b381ffc976841
Ivan Elfimov <ielfimov@gmail.com>
2025-05-11 14:56:06 +0300

    New directory structure
    [Nuxt Docs](https://nuxt.com/docs/getting-started/upgrade#new-directory-structure)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
     autoImport: false,
   },
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ['~/app/assets/css/main.css'],
   mdc: {
     highlight: {
       shikiEngine: 'oniguruma',
