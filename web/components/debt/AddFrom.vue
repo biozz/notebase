823aae9dc5988ed50ff4eadd553e11a70e41864b
Mikhail <meha6699@gmail.com>
2025-05-13 23:27:50 +0300

    feat(debt): extract debt form to separate component

---
+++ web/components/debt/AddFrom.vue
@@ -0,0 +1,51 @@
+<script lang="ts" setup>
+import type { FormSubmitEvent } from '@nuxt/ui'
+import { reactive } from '#imports'
+import { type FormState, formSchema } from '../ItemDebt.vue'
+
+const emit = defineEmits<{
+  (e: 'success', data: FormState): void
+}>()
+
+const state = reactive({
+  amount: 0,
+  comment: '',
+})
+
+function onSubmit(event: FormSubmitEvent<FormState>) {
+  emit('success', event.data)
+}
+</script>
+
+<template>
+  <UForm
+    :state="state"
+    :schema="formSchema"
+    class="mt-2 flex gap-2"
+    @submit="onSubmit"
+  >
+    <UFormField
+      name="amount"
+    >
+      <UInput
+        v-model="state.amount"
+        placeholder="-20000"
+        type="number"
+      />
+    </UFormField>
+
+    <UFormField
+      name="comment"
+    >
+      <UInput
+        v-model="state.comment"
+        placeholder="Comment"
+        type="text"
+      />
+    </UFormField>
+
+    <UButton type="submit">
+      Add
+    </UButton>
+  </UForm>
+</template>
