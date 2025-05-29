7895ed217067e36a7491f1f45754fa6ad1e5b68c
Ivan Elfimov <ielfimov@gmail.com>
2025-05-29 10:57:01 +0300

    Various iOS improvements
    - prevent zoom on focus (16px font-size constraint)
    - disable autocorret and capitalization on input fields

--- web/components/AppFilters.vue
+++ web/components/AppFilters.vue
@@ -22,6 +22,8 @@ defineShortcuts({
       variant="outline"
       placeholder="Query"
       class="w-full"
+      autocapitalize="none"
+      autocorrect="off"
     >
       <template #trailing>
         <UButton
@@ -57,6 +59,9 @@ defineShortcuts({
       color="neutral"
       variant="outline"
       placeholder="inbox/activities/%"
+      class="ios-zoom-fix"
+      autocapitalize="none"
+      autocorrect="off"
     />
   </UButtonGroup>
 
@@ -75,6 +80,15 @@ defineShortcuts({
       color="neutral"
       variant="outline"
       placeholder="debt"
+      class="ios-zoom-fix"
+      autocapitalize="none"
+      autocorrect="off"
     />
   </UButtonGroup>
 </template>
+
+<style lang="css" scoped>
+.ios-zoom-fix {
+  font-size: 16px;
+}
+</style>
